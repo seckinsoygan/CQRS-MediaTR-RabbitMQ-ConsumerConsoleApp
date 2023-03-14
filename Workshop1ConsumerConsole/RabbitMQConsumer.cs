247181c8a38499fd3523daf62dbed9075cabfb1c
seckinsoygan <seckinsoygann@gmail.com>
2023-03-14 13:44:00 +0300

    Routing Key ve Exchange Name düzenlemelerine göre Queue ye mesaj gönderme sağlandı.

--- Workshop1ConsumerConsole/RabbitMQConsumer.cs
+++ Workshop1ConsumerConsole/RabbitMQConsumer.cs
@@ -19,22 +19,30 @@ namespace Workshop1ConsumerConsole
             var factory = new ConnectionFactory() { HostName = "localhost", UserName = "guest", Password = "guest" };
             _connection = factory.CreateConnection();
             _channel = _connection.CreateModel();
-            _channel.QueueDeclare(queue: "Last1",
+            var queueName = new string[] { "QueueA", "QueueB", "QueueC" };
+
+            foreach (var queue in queueName)
+            {
+                _channel.QueueDeclare(queue: queue,
                                   durable: false,
                                   exclusive: false,
                                   autoDelete: false,
                                   arguments: null);
-            _consumer = new EventingBasicConsumer(_channel);
-            _consumer.Received += (model, ea) =>
-            {
-                var body = ea.Body.ToArray();
-                var message = Encoding.UTF8.GetString(body);
-                Console.WriteLine("Received message: {0}", message);
-            };
-            _channel.BasicConsume(queue: "Last1",
-                                  autoAck: true,
-                                  consumer: _consumer);
-            Console.ReadLine();
+                _consumer = new EventingBasicConsumer(_channel);
+
+                _consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var exchangeName = ea.Exchange;
+                    var routingKey = ea.RoutingKey;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("Received message: {0} , RoutingKey = {1} , Exchange = {2}", message,routingKey,exchangeName);
+                };
+                _channel.BasicConsume(queue: queue,
+                                      autoAck: true,
+                                      consumer: _consumer);
+                Console.ReadLine();
+            }
         }
     }
 }
